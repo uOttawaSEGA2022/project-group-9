fab32fb98ad3e946f85705136e8e10173246e7de
Yash Jain <73800643+YashJain04@users.noreply.github.com>
2022-10-01 11:19:47 -0400

    Yash's Login Screen Work Part 1

---
+++ Application/app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button customer = (Button) findViewById(R.id.customerButton);
+        Button chef = (Button) findViewById(R.id.chefButton);
+
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                customerLogin();
+                Toast.makeText(getApplicationContext(), "You are logging in as a customer", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        chef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chefLogin();
+                Toast.makeText(getApplicationContext(), "You are logging in as a chef", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void customerLogin() {
+        Intent custIntent = new Intent(this, customerLoginScreen.class);
+        startActivity(custIntent);
+    }
+
+    public void chefLogin() {
+        Intent chefIntent = new Intent(this, chefLoginScreen.class);
+        startActivity(chefIntent);
+    }
+}
