918719fda7ae4b38ad63f0af0e7ae6e152e57856
Mohamad Kassas <mkass090@uottawa.ca>
2022-10-06 16:36:49 -0400

    Lab 3 calculator Mohamad's design

--- Lab3Calculator/app/src/main/java/com/example/lab3calculator/MainActivity.java
+++ Lab3Calculator/app/src/main/java/com/example/lab3calculator/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,114 +14,134 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn00 = findViewById(R.id.btn00);
+        Button btn01 = findViewById(R.id.btn01);
+        Button btn02 = findViewById(R.id.btn02);
+        Button btn03 = findViewById(R.id.btn03);
+        Button btn04 = findViewById(R.id.btn04);
+        Button btn05 = findViewById(R.id.btn05);
+        Button btn06 = findViewById(R.id.btn06);
+        Button btn07 = findViewById(R.id.btn07);
+        Button btn08 = findViewById(R.id.btn08);
+        Button btn09 = findViewById(R.id.btn09);
+
+        Button btnAdd = findViewById(R.id.btnAdd);
+        Button btnMinus = findViewById(R.id.btnMinus);
+        Button btnDivide = findViewById(R.id.btnDivide);
+        Button btnMultiply = findViewById(R.id.btnMultiply);
+
+        Button btnClear = findViewById(R.id.btnClear);
+        Button btnDot = findViewById(R.id.btnDot);
+
+        Button btnEqual = findViewById(R.id.btnEqual);
+
     }
 
 
 
-    Button btn00 = (Button) findViewById(R.id.btn00);
-    Button btn01 = (Button) findViewById(R.id.btn01);
-    Button btn02 = (Button) findViewById(R.id.btn02);
-    Button btn03 = (Button) findViewById(R.id.btn03);
-    Button btn04 = (Button) findViewById(R.id.btn04);
-    Button btn05 = (Button) findViewById(R.id.btn05);
-    Button btn06 = (Button) findViewById(R.id.btn06);
-    Button btn07 = (Button) findViewById(R.id.btn07);
-    Button btn08 = (Button) findViewById(R.id.btn08);
-    Button btn09 = (Button) findViewById(R.id.btn09);
-
-    Button btnAdd = (Button) findViewById(R.id.btnAdd);
-    Button btnMinus = (Button) findViewById(R.id.btnMinus);
-    Button btnDivide = (Button) findViewById(R.id.btnDivide);
-    Button btnMultiply = (Button) findViewById(R.id.btnMultiply);
-
-    Button btnClear = (Button) findViewById(R.id.btnClear);
-    Button btnDot = (Button) findViewById(R.id.btnDot);
-
-    Button btnEqual = (Button) findViewById(R.id.btnEqual);
 
 
-    public void btn00click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn00click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s0", currentExpression));
     }
 
-    public void btn01click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+
+
+    public void btn01click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s1", currentExpression));
     }
 
-    public void btn02click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn02click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s2", currentExpression));
     }
 
-    public void btn03click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn03click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s3", currentExpression));
     }
 
-    public void btn04click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn04click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s4", currentExpression));
     }
 
-    public void btn05click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn05click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s5", currentExpression));
     }
 
-    public void btn06click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn06click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s6", currentExpression));
     }
 
-    public void btn07click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn07click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s7", currentExpression));
     }
 
-    public void btn08click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn08click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s8", currentExpression));
     }
 
-    public void btn09click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn09click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s9", currentExpression));
     }
 
-    public void btn_minus_click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn_minus_click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s-", currentExpression));
     }
 
-    public void btn_add_click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn_add_click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s+", currentExpression));
     }
 
-    public void btn_divide_click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn_divide_click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s÷", currentExpression));
     }
 
-    public void btn_multiply_click(){
-        EditText displayExpression = (EditText) findViewById(R.id.displayExpression);
+    public void btn_multiply_click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
         String currentExpression = String.valueOf(displayExpression.getText());
         displayExpression.setText(String.format("%s×", currentExpression));
+    }
+
+    public void btn_clear_click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
+        displayExpression.setText("");
+    }
+
+    public void btn_dot_click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
+        String currentExpression = String.valueOf(displayExpression.getText());
+        displayExpression.setText(String.format("%s.", currentExpression));
+    }
+
+    public void btn_equal_click(View view){
+        TextView displayExpression = findViewById(R.id.displayExpression);
+        String currentExpression = String.valueOf(displayExpression.getText());
     }
 
 
