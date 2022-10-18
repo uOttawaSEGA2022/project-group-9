5536c57e904ec0a029156fa888bbe90f6a982b9b
Ishan Phadte <iphad083@uottawa.ca>
2022-10-17 21:50:57 -0400

    remove imports

--- Lab5Firebase/app/src/main/java/com/example/lab5firebase/MainActivity.java
+++ Lab5Firebase/app/src/main/java/com/example/lab5firebase/MainActivity.java
@@ -4,12 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 
 import java.util.ArrayList;
