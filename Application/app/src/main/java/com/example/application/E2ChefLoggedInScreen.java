4b37f88a84d6ab1255650a097937f28559a3585c
Ishan Phadte <iphad083@uottawa.ca>
2022-11-05 23:06:54 -0400

    Update E2ChefLoggedInScreen.java

--- Application/app/src/main/java/com/example/application/E2ChefLoggedInScreen.java
+++ Application/app/src/main/java/com/example/application/E2ChefLoggedInScreen.java
@@ -3,15 +3,26 @@ package com.example.application;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class E2ChefLoggedInScreen extends MainActivity {
-
-    //@SuppressLint("SetTextI18n")
+    FirebaseAuth fAuth;
+    FirebaseDatabase database;
+    DatabaseReference dataRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +31,26 @@ public class E2ChefLoggedInScreen extends MainActivity {
         Button logOff = (Button) findViewById(R.id.logOffButton);
         TextView chefSuspensed = findViewById(R.id.chefSuspensedText);
         TextView chefSuspensedLength = findViewById(R.id.chefSuspensedLengthText);
+
+        fAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Chef");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange (DataSnapshot dataSnapshot){
+                // Get Post object and use the values to update the UI
+                // ..
+
+                Log.d("Getting Data",dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Getting Post failed, log a message
+            }
+        });
+
 
         //check to see if the chef is suspensed in the database
         if(true) {
