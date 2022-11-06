c39ec83da031bbfb874d1b391f40e8ce4d85448b
Ishan Phadte <iphad083@uottawa.ca>
2022-11-06 14:20:24 -0500

    updating calender and comments

--- Application/app/src/main/java/com/example/application/E3AdminLoggedInScreen.java
+++ Application/app/src/main/java/com/example/application/E3AdminLoggedInScreen.java
@@ -1,39 +1,40 @@
 package com.example.application;
-import android.app.AlertDialog;
-import android.app.DatePickerDialog;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import java.util.Calendar;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class E3AdminLoggedInScreen extends MainActivity {
 
-    private DatePickerDialog datePickerDialog;
-    private Button datePicker;
 
-    //https://www.youtube.com/watch?v=qCoidM98zNk&ab_channel=CodeWithCal, some code was taken from here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.e3_admin_logged_in_screen);
 
-        initDatePicker();
 
-        Button datePicker = (Button) findViewById(R.id.datePickerButton);
         Button suspenseChefUntilSelectedDateButton = (Button) findViewById(R.id.suspenseChefUntilSelectedDateButton);
         Button suspenseChefPermanently = (Button) findViewById(R.id.suspenseChefPermanentlyButton);
         Button logOff = (Button) findViewById(R.id.logOffButton);
+        EditText datePicker = findViewById(R.id.datePickerText);
+        TextView errorMessages = findViewById(R.id.errorMessageText);
 
+        authenticator authenticatorObject = new authenticator();
+
+        //Getting Data from loggining
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            Log.d("MSG","In");
+            Log.d("MSG", "In");
         }
 
-        datePicker.setText(getTodaysDate());
+        //Admin needs to be able to look at complaints
+
+
 
         logOff.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,84 +43,19 @@ public class E3AdminLoggedInScreen extends MainActivity {
                 startActivity(mainActivity);            }
         });
 
-
-
-
-    }
-
-    private String getTodaysDate() {
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        return makeDateString(day, month, year);
-    }
-
-    private void initDatePicker() {
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        suspenseChefUntilSelectedDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int month, int day) {
-                month = month + 1;
-                String date = makeDateString(day, month,year);
-                datePicker.setText(date);
-            }
-        };
-        Calendar cal = Calendar.getInstance();
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        month = month + 1;
-        int day = cal.get(Calendar.DAY_OF_MONTH);
+            public void onClick(View view) {
+                //Makes sure the log in is good
+                if (authenticatorObject.validateSuspensionDateFormat(datePicker,errorMessages)) {
+                    //Set Chef Suspensed Date from datePicker
+                    datePicker.getText().toString();
+                    //IDK how to set the data in the data base
+                }
+        }
 
-        int style = AlertDialog.THEME_HOLO_LIGHT;
-        datePickerDialog = new DatePickerDialog(this,style, dateSetListener,year,month,day);
-        datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+        });
 
     }
 
-    private String makeDateString(int day, int month, int year) {
-        return getMonthFormat(month) + " " + day + " " + year;
-    }
-
-    private String getMonthFormat (int month) {
-        if (month == 1) {
-            return "JAN";
-        }
-        if (month == 2) {
-            return "FEB";
-        }
-        if (month == 3) {
-            return "MAR";
-        }
-        if (month == 4) {
-            return "APR";
-        }
-        if (month == 5) {
-            return "MAY";
-        }
-        if (month == 6) {
-            return "JUN";
-        }
-        if (month == 7) {
-            return "JUL";
-        }
-        if (month == 8) {
-            return "AUG";
-        }
-        if (month == 9) {
-            return "SEP";
-        }
-        if (month == 10) {
-            return "OCT";
-        }
-        if (month == 11) {
-            return "NOV";
-        }
-        if (month == 12) {
-            return "DEC";
-        }
-
-        return "JAN";
-
-    }
 }
