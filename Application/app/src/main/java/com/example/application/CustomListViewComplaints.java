08e824e9f4d4149f8f8a1e010c7a33e79104c1d0
Ishan Phadte <iphad083@uottawa.ca>
2022-11-06 21:33:05 -0500

    updated UI for E3

--- Application/app/src/main/java/com/example/application/CustomListViewComplaints.java
+++ Application/app/src/main/java/com/example/application/CustomListViewComplaints.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CustomListViewComplaints extends AppCompatActivity {
+public class CustomListViewComplaints extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
