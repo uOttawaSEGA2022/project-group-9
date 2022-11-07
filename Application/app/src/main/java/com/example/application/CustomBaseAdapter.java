0f1a96fd3e5db9f85efe9bce34e115c695f3595a
Ishan Phadte <iphad083@uottawa.ca>
2022-11-06 20:53:58 -0500

    Create CustomBaseAdapter.java

---
+++ Application/app/src/main/java/com/example/application/CustomBaseAdapter.java
@@ -0,0 +1,39 @@
+package com.example.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class CustomBaseAdapter extends BaseAdapter {
+
+    Context context;
+    String tempListOfChefIDs;
+
+    public CustomBaseAdapter(Context ctx, String[] fruitList, int[] image) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
