c5c4433fc8ef85490e4ae6e46093da29c944db8d
Mohamad Kassas <mkass090@uottawa.ca>
2022-11-18 23:39:18 -0500

    Fixed bug

--- Application/app/src/main/java/com/example/application/AllChefMeals.java
+++ Application/app/src/main/java/com/example/application/AllChefMeals.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 
-import java.sql.DataTruncation;
 import java.util.List;
 
 public class AllChefMeals extends MainActivity{
@@ -37,7 +36,7 @@ public class AllChefMeals extends MainActivity{
         Button modifyOfferedMealsBtn = findViewById(R.id.modifyOfferedMealsBtn);
         Button addMeal = findViewById(R.id.addMealBtn);
 
-        displayChefMeals(allChefMeals);
+        databaseServices.displayChefMeals(allChefMeals, AllChefMeals.this);
 
         addMeal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,112 +70,108 @@ public class AllChefMeals extends MainActivity{
         * */
     }
 
-    public void displayChefMeals(LinearLayout allChefMeals){
-        DatabaseServices databaseServices = new DatabaseServices();
-
-        List<Meal> currentChefMeals = databaseServices.getCurrentChefMeals();
-
-        Log.d("HelloThereBro", "In AllChefMeals.java " + currentChefMeals.toString());
-
-        LayoutInflater inflater = LayoutInflater.from(this);
-
-        for (Meal meal: currentChefMeals){
-            String mealName = meal.getName();
-            String mealCuisine = meal.getCuisine();
-            String mealType = meal.getType();
-            boolean mealIsOffered = meal.getIsOffered();
-
-            View mealTemplate = inflater.inflate(R.layout.meal_template, null);
-
-            TextView mealNameTextView = mealTemplate.findViewById(R.id.mealName);
-            TextView mealCuisineTextView = mealTemplate.findViewById(R.id.mealCuisine);
-            TextView mealTypeTextView = mealTemplate.findViewById(R.id.mealType);
-
-            mealNameTextView.setText(mealName);
-            mealCuisineTextView.setText(mealCuisine);
-            mealTypeTextView.setText(mealType);
-
-            if (!mealIsOffered){
-                TextView mealIsOfferedTextView = mealTemplate.findViewById(R.id.mealIsOfferedTextView);
-                ImageView mealIsOfferedIcon = mealTemplate.findViewById(R.id.mealIsOffered);
-
-                mealIsOfferedTextView.setText("Not Offered");
-                mealIsOfferedIcon.setImageResource(R.drawable.not_offered_unchecked_mark);
-            }
-
-            mealTemplate.setClickable(true);
-            mealTemplate.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent goToEditChefMeal = new Intent(AllChefMeals.this, AddOrEditChefMeal.class);
-                    goToEditChefMeal.putExtra("Meal", meal);
-                    goToEditChefMeal.putExtra("Editing or Adding", "Editing");
-                    startActivity(goToEditChefMeal);
-                }
-            });
-
-            mealTemplate.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View currentMealTemplateView) {
-                    addDeleteDialog(currentMealTemplateView, databaseServices, meal);
-                    return true;
-                }
-            });
-
-            allChefMeals.addView(mealTemplate);
-        }
-    }
-
-    public void addDeleteDialog(View currentMealTemplateView, DatabaseServices databaseServices, Meal meal){
-        TextView isOfferedMealTextView = currentMealTemplateView.findViewById(R.id.mealIsOfferedTextView);
-        TextView mealNameTextView = currentMealTemplateView.findViewById(R.id.mealName);
-        if (!String.valueOf(isOfferedMealTextView.getText()).equals("Offered")){
-            LayoutInflater inflater = LayoutInflater.from(AllChefMeals.this);
-            View popupView = inflater.inflate(R.layout.delete_meal_popup_window, null);
-
-            final AlertDialog alertDialog = new AlertDialog.Builder(AllChefMeals.this, com.google.android.material.R.style.Base_Theme_AppCompat_Dialog_Alert).create();
-            alertDialog.setView(popupView);
-
-            TextView dialogTitleTextView = popupView.findViewById(R.id.deleteMealPopupTitle);
-            dialogTitleTextView.setText("Are you sure you want to remove " + String.valueOf(mealNameTextView.getText()) + " from your menu?");
-
-            Button popupCancelButton = popupView.findViewById(R.id.deleteMealCancelBtn);
-            Button popupDeleteButton = popupView.findViewById(R.id.deleteMealDelteBtn);
-
-            popupCancelButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    alertDialog.dismiss();
-                }
-            });
-
-            popupDeleteButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    LinearLayout allChefMeals = (LinearLayout) currentMealTemplateView.getParent();
-                    allChefMeals.removeView(currentMealTemplateView);
-
-                    // This is where the meal gets deleted from the database
-                    // You can have different approaches to this problem:
-                    // 1: You can unpack the meal object into the different strings and lists it contains
-                    // Then you can search for the element in the database that matches that meal and delete that element from the db
-                    // 2: A different approach (I'm not sure it works, I'll explain why in a bit) is to get all the meals that are currently in the linear layout
-                    // Unpack them all and override the whole meal section of the database
-                    // This may not work since, in this code block, the methods only see one meal at a time, notice how all of this is in a for loop
-                    // So they may not see the rest of the meal templates that are in the linear layout with the current meal template
-                    // And this approach is also not efficient, but may work, so do with it as you wish
-                    databaseServices.removeMeal(meal);
-
-                    alertDialog.dismiss();
-                }
-            });
-
-            alertDialog.show();
-            alertDialog.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "This meal is currently offered, cannot remove from menu", Toast.LENGTH_LONG).show();
-        }
-    }
+//    public void displayChefMeals(LinearLayout allChefMeals){
+//
+//
+//        LayoutInflater inflater = LayoutInflater.from(AllChefMeals.this);
+//
+//        for (Meal meal: mealList){
+//            String mealName = meal.getName();
+//            String mealCuisine = meal.getCuisine();
+//            String mealType = meal.getType();
+//            boolean mealIsOffered = meal.getIsOffered();
+//
+//            View mealTemplate = inflater.inflate(R.layout.meal_template, null);
+//
+//            TextView mealNameTextView = mealTemplate.findViewById(R.id.mealName);
+//            TextView mealCuisineTextView = mealTemplate.findViewById(R.id.mealCuisine);
+//            TextView mealTypeTextView = mealTemplate.findViewById(R.id.mealType);
+//
+//            mealNameTextView.setText(mealName);
+//            mealCuisineTextView.setText(mealCuisine);
+//            mealTypeTextView.setText(mealType);
+//
+//            if (!mealIsOffered){
+//                TextView mealIsOfferedTextView = mealTemplate.findViewById(R.id.mealIsOfferedTextView);
+//                ImageView mealIsOfferedIcon = mealTemplate.findViewById(R.id.mealIsOffered);
+//
+//                mealIsOfferedTextView.setText("Not Offered");
+//                mealIsOfferedIcon.setImageResource(R.drawable.not_offered_unchecked_mark);
+//            }
+//
+//            mealTemplate.setClickable(true);
+//            mealTemplate.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent goToEditChefMeal = new Intent(AllChefMeals.this, AddOrEditChefMeal.class);
+//                    goToEditChefMeal.putExtra("Meal", meal);
+//                    goToEditChefMeal.putExtra("Editing or Adding", "Editing");
+//                    startActivity(goToEditChefMeal);
+//                }
+//            });
+//
+//            mealTemplate.setOnLongClickListener(new View.OnLongClickListener() {
+//                @Override
+//                public boolean onLongClick(View currentMealTemplateView) {
+//                    addDeleteDialog(currentMealTemplateView, databaseServices, meal);
+//                    return true;
+//                }
+//            });
+//
+//            allChefMeals.addView(mealTemplate);
+//        }
+//    }
+//
+//    public void addDeleteDialog(View currentMealTemplateView, DatabaseServices databaseServices, Meal meal){
+//        TextView isOfferedMealTextView = currentMealTemplateView.findViewById(R.id.mealIsOfferedTextView);
+//        TextView mealNameTextView = currentMealTemplateView.findViewById(R.id.mealName);
+//        if (!String.valueOf(isOfferedMealTextView.getText()).equals("Offered")){
+//            LayoutInflater inflater = LayoutInflater.from(AllChefMeals.this);
+//            View popupView = inflater.inflate(R.layout.delete_meal_popup_window, null);
+//
+//            final AlertDialog alertDialog = new AlertDialog.Builder(AllChefMeals.this, com.google.android.material.R.style.Base_Theme_AppCompat_Dialog_Alert).create();
+//            alertDialog.setView(popupView);
+//
+//            TextView dialogTitleTextView = popupView.findViewById(R.id.deleteMealPopupTitle);
+//            dialogTitleTextView.setText("Are you sure you want to remove " + String.valueOf(mealNameTextView.getText()) + " from your menu?");
+//
+//            Button popupCancelButton = popupView.findViewById(R.id.deleteMealCancelBtn);
+//            Button popupDeleteButton = popupView.findViewById(R.id.deleteMealDelteBtn);
+//
+//            popupCancelButton.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    alertDialog.dismiss();
+//                }
+//            });
+//
+//            popupDeleteButton.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    LinearLayout allChefMeals = (LinearLayout) currentMealTemplateView.getParent();
+//                    allChefMeals.removeView(currentMealTemplateView);
+//
+//                    // This is where the meal gets deleted from the database
+//                    // You can have different approaches to this problem:
+//                    // 1: You can unpack the meal object into the different strings and lists it contains
+//                    // Then you can search for the element in the database that matches that meal and delete that element from the db
+//                    // 2: A different approach (I'm not sure it works, I'll explain why in a bit) is to get all the meals that are currently in the linear layout
+//                    // Unpack them all and override the whole meal section of the database
+//                    // This may not work since, in this code block, the methods only see one meal at a time, notice how all of this is in a for loop
+//                    // So they may not see the rest of the meal templates that are in the linear layout with the current meal template
+//                    // And this approach is also not efficient, but may work, so do with it as you wish
+//                    databaseServices.removeMeal(meal);
+//
+//                    alertDialog.dismiss();
+//                }
+//            });
+//
+//            alertDialog.show();
+//            alertDialog.getWindow().setLayout(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+//        }
+//        else{
+//            Toast.makeText(getApplicationContext(), "This meal is currently offered, cannot remove from menu", Toast.LENGTH_LONG).show();
+//        }
+//    }
 
 }
--- Application/app/src/main/java/com/example/application/ModifyOfferedMeals.java
+++ Application/app/src/main/java/com/example/application/ModifyOfferedMeals.java
@@ -2,7 +2,6 @@ package com.example.application;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +22,7 @@ public class ModifyOfferedMeals extends MainActivity {
         Intent intent = getIntent();
 
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
@@ -42,66 +41,67 @@ public class ModifyOfferedMeals extends MainActivity {
         });
     }
 
-    public void displayChefMealsForModifyingOfferedMeals(LinearLayout allChefMeals){
+    public void displayChefMealsForModifyingOfferedMeals(LinearLayout allChefMeals) {
         DatabaseServices databaseServices = new DatabaseServices();
 
-        List<Meal> currentChefMeals = databaseServices.getCurrentChefMeals();
+        databaseServices.displayChefMeals(allChefMeals, ModifyOfferedMeals.this);
 
-        LayoutInflater inflater = LayoutInflater.from(this);
-
-        for (Meal meal: currentChefMeals) {
-            String mealName = meal.getName();
-            String mealCuisine = meal.getCuisine();
-            String mealType = meal.getType();
-            boolean mealIsOffered = meal.getIsOffered();
-
-            View mealTemplate = inflater.inflate(R.layout.meal_template, null);
-
-            TextView mealNameTextView = mealTemplate.findViewById(R.id.mealName);
-            TextView mealCuisineTextView = mealTemplate.findViewById(R.id.mealCuisine);
-            TextView mealTypeTextView = mealTemplate.findViewById(R.id.mealType);
-
-            mealNameTextView.setText(mealName);
-            mealCuisineTextView.setText(mealCuisine);
-            mealTypeTextView.setText(mealType);
-
-            if (!mealIsOffered) {
-                TextView mealIsOfferedTextView = mealTemplate.findViewById(R.id.mealIsOfferedTextView);
-                ImageView mealIsOfferedIcon = mealTemplate.findViewById(R.id.mealIsOffered);
-
-                mealIsOfferedTextView.setText("Not Offered");
-                mealIsOfferedIcon.setImageResource(R.drawable.not_offered_unchecked_mark);
-            }
-
-            mealTemplate.setClickable(true);
-            mealTemplate.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    TextView mealIsOfferedTextView = mealTemplate.findViewById(R.id.mealIsOfferedTextView);
-                    ImageView mealIsOfferedIcon = mealTemplate.findViewById(R.id.mealIsOffered);
-                    boolean mealIsOffered = meal.getIsOffered();
-                    if (mealIsOffered){
-                        meal.setOffered(false);
-                        mealIsOfferedTextView.setText("Not Offered");
-                        mealIsOfferedIcon.setImageResource(R.drawable.not_offered_unchecked_mark);
-                    }
-                    else{
-                        meal.setOffered(true);
-                        mealIsOfferedTextView.setText("Offered");
-                        mealIsOfferedIcon.setImageResource(R.drawable.offered_checkmark);
-                    }
-
-                    // Editing the current meal in the database, but only changing the offered status
-                    // There's an alternative solution to this since realtime database may crash if the cook spams clicking
-                    // We can get the offered status of each meal when the finish button is clicked
-                    // The only problem is that the meal object is not related in any way to the UI elements
-                    // The UI elements have access to the meal object at creation only and the meal never has access to the UI elements
-                    // So getting meals out of the UI elements will be a really challenging task which will require heavy modification to the whole system
-                    databaseServices.updateOrAddChefMeal(meal, "Editing");
-                }
-            });
-
-            allChefMeals.addView(mealTemplate);
-        }
+//        LayoutInflater inflater = LayoutInflater.from(this);
+//
+//        for (Meal meal: currentChefMeals) {
+//            String mealName = meal.getName();
+//            String mealCuisine = meal.getCuisine();
+//            String mealType = meal.getType();
+//            boolean mealIsOffered = meal.getIsOffered();
+//
+//            View mealTemplate = inflater.inflate(R.layout.meal_template, null);
+//
+//            TextView mealNameTextView = mealTemplate.findViewById(R.id.mealName);
+//            TextView mealCuisineTextView = mealTemplate.findViewById(R.id.mealCuisine);
+//            TextView mealTypeTextView = mealTemplate.findViewById(R.id.mealType);
+//
+//            mealNameTextView.setText(mealName);
+//            mealCuisineTextView.setText(mealCuisine);
+//            mealTypeTextView.setText(mealType);
+//
+//            if (!mealIsOffered) {
+//                TextView mealIsOfferedTextView = mealTemplate.findViewById(R.id.mealIsOfferedTextView);
+//                ImageView mealIsOfferedIcon = mealTemplate.findViewById(R.id.mealIsOffered);
+//
+//                mealIsOfferedTextView.setText("Not Offered");
+//                mealIsOfferedIcon.setImageResource(R.drawable.not_offered_unchecked_mark);
+//            }
+//
+//            mealTemplate.setClickable(true);
+//            mealTemplate.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    TextView mealIsOfferedTextView = mealTemplate.findViewById(R.id.mealIsOfferedTextView);
+//                    ImageView mealIsOfferedIcon = mealTemplate.findViewById(R.id.mealIsOffered);
+//                    boolean mealIsOffered = meal.getIsOffered();
+//                    if (mealIsOffered){
+//                        meal.setOffered(false);
+//                        mealIsOfferedTextView.setText("Not Offered");
+//                        mealIsOfferedIcon.setImageResource(R.drawable.not_offered_unchecked_mark);
+//                    }
+//                    else{
+//                        meal.setOffered(true);
+//                        mealIsOfferedTextView.setText("Offered");
+//                        mealIsOfferedIcon.setImageResource(R.drawable.offered_checkmark);
+//                    }
+//
+//                    // Editing the current meal in the database, but only changing the offered status
+//                    // There's an alternative solution to this since realtime database may crash if the cook spams clicking
+//                    // We can get the offered status of each meal when the finish button is clicked
+//                    // The only problem is that the meal object is not related in any way to the UI elements
+//                    // The UI elements have access to the meal object at creation only and the meal never has access to the UI elements
+//                    // So getting meals out of the UI elements will be a really challenging task which will require heavy modification to the whole system
+//                    databaseServices.updateOrAddChefMeal(meal, "Editing");
+//                }
+//            });
+//
+//            allChefMeals.addView(mealTemplate);
+//        }
+//    }
     }
 }
